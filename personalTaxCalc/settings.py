9720c58282a45fde355b8d583e741e28bcb27438
maclyn02 <mac.afonso@yahoo.com>
2019-03-24 08:04:22 +0000

    Update settings.py
    security bug fixed

--- personalTaxCalc/settings.py
+++ personalTaxCalc/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'gkg!y(p-@to=c7hrk&!*ux_33siqelyjs#0s_%m=gb^3zjz2@z'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
