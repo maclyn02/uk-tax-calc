deea3a0bb5cb6554f6ee1b186ce678e8dcd9228e
maclyn02 <mac.afonso@yahoo.com>
2019-03-24 09:25:10 +0000

    Updated settings.py

--- personalTaxCalc/settings.py
+++ personalTaxCalc/settings.py
@@ -121,7 +121,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, 'static'))
+STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, 'static'),)
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
