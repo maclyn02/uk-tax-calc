210c9bfd0496de7a9bafcbd65a5dc17849fc900f
maclyn02 <mac.afonso@yahoo.com>
2020-01-12 23:46:02 +0000

    bug fix

--- personalTaxCalc/settings.py
+++ personalTaxCalc/settings.py
@@ -123,7 +123,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static/'),
+    os.path.join(PROJECT_ROOT, "personalTaxCalc","static"),
     )
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
