95cdfa37e22e20582300b73c490079b5c1f733e9
maclyn02 <mac.afonso@yahoo.com>
2020-01-12 23:50:23 +0000

    bug fix

--- personalTaxCalcApp/urls.py
+++ personalTaxCalcApp/urls.py
@@ -3,6 +3,5 @@ from .import views
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
-    patterns('',url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root':settings.STATIC_ROOT)}),)
 
 ]
