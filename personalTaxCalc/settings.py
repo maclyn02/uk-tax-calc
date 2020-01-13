db7c22ae7a7de4883e1ebd2f93de8176819d5617
maclyn02 <mac.afonso@yahoo.com>
2020-01-13 12:41:53 +0000

    bug fix

--- personalTaxCalc/settings.py
+++ personalTaxCalc/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = 'personalTaxCalc/static/'
 #STATICFILES_DIRS = (
 #    os.path.join(PROJECT_ROOT, "personalTaxCalc","static"),
 #    )
