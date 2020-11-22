7f5fcc52161909b36bb1c6f5ea24986cd8bf0491
maclyn02 <mac.afonso@yahoo.com>
2020-11-22 12:44:22 +0000

    updated structure and formulae

--- personalTaxCalcApp/static/js/genResultsTemplate.js
+++ personalTaxCalcApp/static/js/genResultsTemplate.js
@@ -1,31 +1,62 @@
 function generateHourlyTemplate(){
   document.getElementById('results-container').innerHTML = `
-      Rate Per Hour: <input type="text" id="basicHrRate" value="8.50"/>   X  <input type="text" id="numHours" value="160"/> Hours
-      <br>
-      Overtime Rate Per Hour: <input type="text" id="overtimeRate" value="1.5"/> times   X   <input type="text" id="overtimeHours" value="0"/> Hours
-      <br>
-      Additional Overtime Rate Per Hour: <input type="text" id="additionalOvertimeRate" value="2"/> times   X   <input type="text" id="additionalOvertimeHours" value="0"/> Hours
-      <br>
-      Additional Compensation: <input type="text" id="additionalCompensation" value="0"/>
-      <br>
-      Additional Compensation(NO NI DEDUCTED): <input type="text" id="additionalCompensationNONI" value="0"/>
+      <table>
+        <tr>
+          <td class="label">Rate Per Hour:</td>
+          <td class="content"><input type="text" id="basicHrRate" value="8.50"/>   X  <input type="text" id="numHours" value="160"/> Hours</td>
+        </tr>
+        <tr>
+          <td class="label">Overtime Rate Per Hour:</td>
+          <td class="content"><input type="text" id="overtimeRate" value="1.5"/> times   X   <input type="text" id="overtimeHours" value="0"/> Hours</td>
+        </tr>
+        <tr>
+          <td class="label">Additional Overtime Rate Per Hour:</td>
+          <td class="content"><input type="text" id="additionalOvertimeRate" value="2"/> times   X   <input type="text" id="additionalOvertimeHours" value="0"/> Hours</td>
+        </tr>
+        <tr>
+          <td class="label">Additional Compensation:</td>
+          <td class="content"><input type="text" id="additionalCompensation" value="0"/></td>
+        </tr>
+        <tr>
+          <td class="label">Additional (Non-NI) Compensation:</td>
+          <td class="content"><input type="text" id="additionalCompensationNONI" value="0"/></td>
+        </tr>
+      </table>
     `
 }
 function generateMonthlyTemplate(){
   document.getElementById('results-container').innerHTML = `
-      Monthly Base Pay: <input type="text" id="monthlyBaseSalary" value="1833"/>
-      <br>
-      Additional Compensation: <input type="text" id="additionalCompensation" value="0"/>
-      <br>
-      Additional Compensation(NO NI DEDUCTED): <input type="text" id="additionalCompensationNONI" value="0"/>
+      <table>
+        <tr>
+          <td class="label">Monthly Base Pay:</td>
+          <td class="content"><input type="text" id="monthlyBaseSalary" value="1833"/></td>
+        </tr>
+        <tr>
+          <td class="label">Additional Compensation:</td>
+          <td class="content"><input type="text" id="additionalCompensation" value="0"/></td>
+        </tr>
+        <tr>
+          <td class="label">Additional (Non-NI) Compensation:</td>
+          <td class="content"><input type="text" id="additionalCompensationNONI" value="0"/></td>
+        </tr>
+      </table>
     `
 }
 function generateAnnualTemplate(){
-  document.getElementById('results-container').innerHTML =`
-      Annual Base Pay: <input type="text" id="annualBaseSalary" value="22000"/>
-      <br>
-      Additional Compensation: <input type="text" id="additionalCompensation" value="0"/>
-      <br>
-      Additional Compensation(NO NI DEDUCTED): <input type="text" id="additionalCompensationNONI" value="0"/>
+  document.getElementById('results-container').innerHTML =`       
+      <table>
+        <tr>
+          <td class="label">Annual Base Pay:</td>
+          <td class="content"><input type="text" id="annualBaseSalary" value="22000"/></td>
+        </tr>
+        <tr>
+          <td class="label">Additional Compensation:</td>
+          <td class="content"><input type="text" id="additionalCompensation" value="0"/></td>
+        </tr>
+        <tr>
+          <td class="label">Additional (Non-NI) Compensation:</td>
+          <td class="content"><input type="text" id="additionalCompensationNONI" value="0"/></td>
+        </tr>
+      </table>
    `
 }
